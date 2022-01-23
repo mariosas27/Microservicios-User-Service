9ef1c3363d57ab353b4d67a4c358c202b6138149
Juan Mario Sastre Cuahutle <mariosas27@gmail.com>
2022-01-23 13:39:56 -0600

    Pimmer commit, se agregaron todos los archivos

---
+++ src/main/java/com/uam/azc/user/VO/ResponseTemplateVO.java
@@ -0,0 +1,15 @@
+package com.uam.azc.user.VO;
+
+import com.uam.azc.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+}
---
+++ src/main/java/com/uam/azc/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.uam.azc.user.repository;
+import com.uam.azc.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User,Long> {
+    User findByUserId(Long userId);
+}
